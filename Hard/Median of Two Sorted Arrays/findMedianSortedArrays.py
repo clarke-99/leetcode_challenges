3d154913dc8513df020548ef6a5ee06245963ade
clarke-99 <165060959+clarke-99@users.noreply.github.com>
2024-08-20 13:49:15 +0100

    Create findMedianSortedArrays.py

---
+++ Hard/Median of Two Sorted Arrays/findMedianSortedArrays.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        for nums in nums2:
+            nums1.append(nums)
+
+        nums1.sort()
+        l = float((len(nums1)+1))/2
+        if '.5' in str(l):
+            l1 = int(l - 0.5) -1
+            l2 = int(l + 0.5) -1
+            return float(nums1[l1] + nums1[l2])/2
+            m = (nums1[l1]+nums1[l2])/2
+        else:
+            m = nums1[int(l-1)]
+        return m
+
