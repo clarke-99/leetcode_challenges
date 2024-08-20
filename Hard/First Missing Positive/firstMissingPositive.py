e38f0acf7e4d524b8304752900ba5f66af660a28
clarke-99 <165060959+clarke-99@users.noreply.github.com>
2024-08-20 17:28:22 +0100

    Update firstMissingPositive.py

--- Hard/First Missing Positive/firstMissingPositive.py
+++ Hard/First Missing Positive/firstMissingPositive.py
@@ -4,25 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        missing = None
-        s = sorted(nums)
-        i = 0
-        # return s
-        while i < len(s):
-            if 1 not in s:
-                return 1
-            if i + 1 < len(s):
-                j = i +1
-                if s[i] == s[j]:
-                    pass
-                elif s[i] > -1 and (s[i] + 1 != s[j]):
-                    return s[i] + 1
-                
-            
-            i += 1
-    
-        return s[-1] +1
-
-
+        n = len(nums)
         
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                correct_index = nums[i] - 1
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
         
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+
+        return n + 1
