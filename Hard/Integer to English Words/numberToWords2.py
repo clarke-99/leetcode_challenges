cde98753843b9f2209dbbf1b4e4659b5c3ff6b5d
clarke-99 <165060959+clarke-99@users.noreply.github.com>
2024-08-28 13:51:58 +0100

    Update numberToWords2.py
    This is the working solution

--- Hard/Integer to English Words/numberToWords2.py
+++ Hard/Integer to English Words/numberToWords2.py
@@ -4,80 +4,49 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        nums = {0:['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'], 
-        1:[['', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'], ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']], 2:'Hundred', 3:'Thousand', 6:'Million', 9:'Billion'}
-
-        def check_dig(i):
-            while i > 2:
-                i -= 3
-            return i
-
-        def add_suffix(x, values):
-            if any(values) > 0:
-                if x == 3:
-                    return 'Thousand'
-                elif x == 6:
-                    return 'Million'
-                elif x == 9:
-                    return 'Billion'
-
-
-        num_list = [int(num) for num in str(num)]
-        i,l = 0, len(num_list)-1
-
-        if len(num_list) == 1 and num_list[0]==0:
+        if num == 0:
             return 'Zero'
 
+        num_list = [num for num in str(num)]
 
-        # 1 2 3 4 0 
-    # i   0 1 2 3 4 
-    # x   4 3 2 1 0 
+        below_20 = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+        'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 
+        'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+        tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty',
+        'Seventy', 'Eighty', 'Ninety']
+        suffix = {3:'Thousand', 6:'Million', 9:'Billion'}
 
-        word = []
-
-        while i <= l:
-            x = l - i
-            s = check_dig(x)
-            if num_list[i] == 0:
-                if x in (3, 6, 9):
-                    j = x
-                    sub = 3
-                    while j - sub < 0:
-                        sub -= 1                        
-                    suffix = add_suffix(x, num_list[i-sub:i])
-                    if suffix:
-                        word.append(suffix)
-            else:
-                if s in (2, 0):
-                    word.append(nums[0][num_list[i]])
-                    if s==2 and num_list[i]!=0:
-                        word.append(nums[s])
+        def find_num(n):
+            if n < 20:
+                return below_20[n] + ' '
+            elif n < 100:
+                if n %10 == 0:
+                    return tens[n//10] + ' '
                 else:
-                    if num_list[i] > 1:
-                        word.append(nums[1][1][num_list[i]])
-                # elif num_list[i] ==0:
-                #     pass
-                    else:
-                        if num_list[i+1] != 0:
-                            word.append(nums[1][0][num_list[i+1]])
-                        else:
-                            word.append(nums[1][1][num_list[i]])
-                        i +=1
-                        x-=1
-                
-                if x in (3, 6, 9):
-                    j = x
-                    sub = 3
-                    while j - sub < 0:
-                        sub -= 1   
-                    suffix = add_suffix(x, num_list[i-sub:i])
-                    if suffix:
-                        word.append(suffix)
-                
-            i += 1
+                    return tens[n//10] + ' ' + find_num(n%10)
+            else:
+                if n%100 ==0:
+                    return below_20[n // 100] + ' Hundred '
+                else:
+                    return below_20[n // 100] + ' Hundred ' + find_num(n % 100)
 
-        # word = ' '.join(word)
-        return ' '.join(word)
-        # return word
-        
-        
+    
+        word = ''
+        while num_list:
+            l = len(num_list)
+            if l % 3 != 0:
+                chunk = l % 3
+            else:
+                chunk = 3
+            current = int(''.join(num_list[0:chunk]))
+            if current > 0:
+                word += find_num(current)
+                if l - chunk in suffix:
+                    word += suffix[l-chunk] + ' '
+
+            num_list=num_list[chunk:]
+
+        return word.strip()
+
+
+
